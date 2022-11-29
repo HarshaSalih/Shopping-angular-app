c8380a95c09a356196b75b62b9cb8fe44efba9cd
HarshaSalih <harshasalih2000@gmail.com>
2022-11-29 21:37:20 +0530

    commits

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   constructor(private http:HttpClient) { }
-  fetchCourses=()=>
+  fetchProduct=()=>
   {
     return this.http.get("https://fakestoreapi.com/products")
   }
